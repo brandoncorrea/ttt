440a8dfd1c7766b927ca2343692db7beb866499b
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-03-26 18:48:47 -0400

    Create user input parser

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func ParseUserInput(input string) [2]int {
+	var fields = strings.Fields(strings.ReplaceAll(input, ",", " "))
+	if len(fields) != 2 {
+		return [2]int{-1, -1}
+	}
+
+	var move [2]int
+	for i := range move {
+		var _, err = fmt.Sscan(fields[i], &move[i])
+		if err != nil {
+			return [2]int{-1, -1}
+		}
+	}
+
+	return move
+}
 
 func main() {
 	fmt.Println("Tic Tac Toe")
--- main_test.go
+++ main_test.go
@@ -1,7 +1,45 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"ttt/core"
+)
 
-func TestExecutesMain(t *testing.T) {
-	main()
+func TestParsesBadInput(t *testing.T) {
+	var disallowedInputs = []string{
+		"",
+		"\n",
+		"\r\n",
+		" ",
+		"a",
+		"a b",
+		"a b c",
+		"1 a",
+		"a 1",
+		"1",
+		"1 2 3",
+		"1 - 3",
+	}
+	var badResult = [2]int{-1, -1}
+	for _, input := range disallowedInputs {
+		assert.Equal(t, badResult, ParseUserInput(input))
+	}
+}
+
+func TestParsesFirstCell(t *testing.T) {
+	var acceptableFormats = []string{
+		"%d, %d",
+		"%d %d",
+		"%d,%d",
+		"  \t  %d  \n  %d \r ",
+	}
+
+	core.ForIndices(func(row int, column int) {
+		var cell = [2]int{row, column}
+		for _, format := range acceptableFormats {
+			assert.Equal(t, cell, ParseUserInput(fmt.Sprintf(format, row, column)))
+		}
+	})
 }
